fcc6798816373fe030d1ad38999649ec94a107b9
Sylvie Trouilhet <sylvie.trouilhet@irit.fr>
2023-02-07 19:36:46 +0100

    version graphique

--- app/f.py
+++ app/f.py
@@ -1,6 +1,28 @@
-''' doc
+import random
 
-'''
-def essai():
-    print("essai")
-    return True
+def propositionJoueur(i):
+    print(i)
+    tab=["ciseaux", "feuille", "pierre"]
+    if i >=0 and i <len(tab) :
+        return tab[i]
+    else :
+        return None
+
+def propositionMachine():
+    tab=["ciseaux", "feuille", "pierre"]
+    nb=random.randint(0,2)
+    return(tab[nb])
+
+def shifumi(p1, p2):
+    rep=1
+    if p1 not in ["ciseaux", "feuille", "pierre"]:
+        rep=None
+    elif p2 not in ["ciseaux", "feuille", "pierre"]:
+        rep=None
+    elif p1==p2: 
+        rep=-1
+    elif p1=="pierre" and p2=="ciseaux" or p1=="feuille" and p2=="pierre" or p1=="ciseaux" and p2=="feuille":
+        rep=0
+    return rep
+
+    
--- app/ff.py
+++ app/ff.py
@@ -1,14 +1,31 @@
 from tkinter import *
+from f import *
 
-from f import essai
 #callback
 def ok():
-    essai()
+    p1=propositionJoueur(l.curselection()[0])
+    p2=propositionMachine()
+    r=shifumi(p1,p2)
+    print(p1, p2, r)
+    resultat="tu as gagné"
+    if r==-1:
+        resultat="pas de gagnant"
+    elif r==1:
+        resultat="tu as perdu"
+    print(resultat)        
     f1.destroy()
+
+
+
 #création de l'IHM
 f1 = Tk()
-f1.title("Premier exemple")
-b1=Button(f1,text='Cliquez',command=ok)
+f1.title("Shifumi")
+b1=Button(f1,text='Jouer',command=ok)
+l=Listbox(f1)
+l.insert(0,"ciseaux")
+l.insert(1, "feuille")
+l.insert(2, "pierre")
+l.pack();
 b1.pack(side=LEFT,padx=10, pady=10)
 #boucle infinie d'attente
 f1.mainloop()
